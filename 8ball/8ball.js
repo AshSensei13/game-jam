ecfb238b92d561fe12832ba1656529fad90c4270
AshSensei13 <ashley.bradley@hotmail.co.uk>
2026-08-14 12:01:39 +0100

    updated comments on 8ball.js

--- 8ball/8ball.js
+++ 8ball/8ball.js
@@ -1,6 +1,6 @@
 //Magic 8 ball game
 
-//Add function to collect name of user
+//Add operator to collect name of user
 let userName = ''
 userName ? console.log(`Hello ${userName}`)
 : console.log('Hello!');
