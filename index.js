92369a3c7f0dc0dbecdf0a39c77bac86668687ec
AshSensei13 <ashley.bradley@hotmail.co.uk>
2026-08-14 09:30:07 +0100

    created computerChoice function

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ const getUserChoice = userInput => {
     }
 }
 
+//Create function to get computer choice
+const getComputerChoice = () => {
+    let randNum = Math.floor(Math.random() * 3)
+    switch (randNum) {
+        case 0: return 'rock'
+        break
+        case 1: return 'paper'
+        break
+        case 2: return 'scissors'
+        break
+    }
+}
