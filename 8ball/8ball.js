19f74e626e4b201c5e09412f6628d99adecccb20
AshSensei13 <ashley.bradley@hotmail.co.uk>
2026-08-14 12:12:09 +0100

    created userQuestion variable

--- 8ball/8ball.js
+++ 8ball/8ball.js
@@ -5,3 +5,6 @@ let userName = ''
 userName ? console.log(`Hello ${userName}`)
 : console.log('Hello!');
 
+//Added userQuestion variable
+let userQuestion = 'Am I a good programmer?'
+console.log(`${userName} asked ${userQuestion}`);
